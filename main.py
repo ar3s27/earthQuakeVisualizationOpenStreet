6db4a47e214f6342f0253605d6062d76fdf3b84d
Veysel <105015018+ar3s27@users.noreply.github.com>
2024-05-15 13:49:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import branca
 from flask import Flask, render_template
 import requests
 import folium
+from calculations import earthquake_percentage,earthquake_location
 
 app = Flask(__name__)
 
@@ -9,13 +11,13 @@ def fetch_earthquake_data(url):
     if response.status_code == 200:
         return response.json()
     else:
-        print("Failed to fetch earthquake data:", response.status_code)
+        print("Deprem Verileri Alınamadı:", response.status_code)
         return None
 
 def create_map_with_markers(data):
-    # Initialize the map
+    # Haritayı başlatır
     harita = folium.Map(location=[38.3335, 35.1500], zoom_start=6)
-    # Get the most recent earthquake data
+    # En güncel deprem verilerini alır
     most_recent_quake = None
     most_recent_date = ""
     for quake_id, entry in data.items():
@@ -24,45 +26,62 @@ def create_map_with_markers(data):
             most_recent_date = date
             most_recent_quake = entry
 
-    # Iterate over each earthquake event in the data
+    # Verideki her deprem olayı için işlem yapar
     for quake_id, entry in data.items():
         enlem = float(entry['latitude'])
         boylam = float(entry['longitude'])
         buyukluk = entry['magnitude']
+        derinlik = entry['depth']
         lokasyon = entry['location']
 
-        # Create popup content for the marker
-        popup_content = f"Lokasyon: {lokasyon}<br>Enlem: {enlem}<br>Boylam: {boylam}<br>Büyüklük: {buyukluk}"
+        # Pencere içeriğini oluşturur
+        html = f"""<h4>Lokasyon: {lokasyon}<br>
+        Enlem: {enlem}<br>
+        Boylam: {boylam}<br>
+        Büyüklük: {buyukluk}<br>
+        Derinlik: {derinlik}</h4>"""
 
-        # Determine marker color based on whether it is the most recent earthquake
+        # En son depreme göre işaret rengini belirler
         if entry == most_recent_quake:
-            marker_color = 'red'  # You can choose any color you prefer for the most recent earthquake marker
+            marker_color = 'red'  #  En son deprem işareti için istediğiniz rengi seçebilirsiniz
         else:
             marker_color = 'blue'
+        
+        iframe = branca.element.IFrame(html=html, width=250, height=125)
+        popup = folium.Popup(iframe, max_width=500)
+        # Haritaya işaret ekler
+        folium.Marker([enlem, boylam], popup=popup, icon=folium.Icon(color=marker_color)).add_to(harita)
 
-        # Add marker to the map
-        folium.Marker([enlem, boylam], popup=popup_content, icon=folium.Icon(color=marker_color)).add_to(harita)
+    # Daire ekler
+    circle = folium.Circle(
+        location=earthquake_location(),
+        radius=400000,
+        color="red",
+        fill=True,
+        fill_color="red",
+        tooltip=f"<h4>Bu Bölgede 3.5 Üzeri Deprem Olma Olasığı %{earthquake_percentage()}</h4>",
+    ).add_to(harita)
     
     return harita
 
 @app.route('/')
 def index():
-    # URL for earthquake data
+    # Deprem veri URL'si
     url = "https://deprem-gorsellestirme.vercel.app"
 
-    # Fetch earthquake data
+    # Deprem verilerini alır
     earthquake_data = fetch_earthquake_data(url)
 
     if earthquake_data:
-        # Create the map with markers
+        # İşaretli haritayı oluşturur
         map_with_markers = create_map_with_markers(earthquake_data)
 
-        # Save the map as an HTML string
+        # Haritayı bir HTML dizesi olarak kaydeder
         map_html = map_with_markers._repr_html_()
 
         return render_template('index.html', map_html=map_html)
     else:
-        return "Failed to fetch earthquake data."
+        return "Deprem verileri alınamadı."
 
 if __name__ == '__main__':
     app.run(debug=True)
