43d8adafdfcdbd2a1610f8c05631828627f8bb9f
SilanTheEngineer <silan.cakan73@gmail.com>
2024-06-05 10:43:20 +0300

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,7 +1,7 @@
 import random as rnd
 
 def earthquake_percentage():
-    percentage = rnd.randint(20,60)
+    percentage = rnd.uniform(20.0, 40.0)
     return percentage
 
 def earthquake_location():
